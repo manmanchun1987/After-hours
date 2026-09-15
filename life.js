07a8087240c5a0c4f5777bcbbae38a9b8294d090
manmanchun1987 <manmanchun1987@gmail.com>
2026-09-15 15:11:06 +0800

    CODE 2026-09-15 15:10 HKT cache live34 keep photoreal CLIP path

--- life.js
+++ life.js
@@ -1,4 +1,4 @@
-/* live33 cache */
+/* live34 cache */
 (function () {
   var BASE = location.pathname.indexOf("/After-hours") === 0 ? "/After-hours" : ".";
   var FILES = { alex: BASE + "/data/alex.json", morgan: BASE + "/data/morgan.json", sam: BASE + "/data/sam.json" };
